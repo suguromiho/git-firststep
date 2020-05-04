f8c8c4072311a8359257c3dd9dab08288b6465a0
勝呂 美歩 <suguromiho@suguro-no-MacBook-Air.local>
2020-05-04 16:49:37 +0900

    update

--- yamanote.js
+++ yamanote.js
@@ -14,13 +14,26 @@ async function getRequest() {
   let response;
   try {
     response = await axios.get('https://transit.yahoo.co.jp/traininfo/detail/21/0/');
+    // console.log(response.data);
     let html = response.data;
     html = html.replace(/\r?\n/g,""); //整形1: 改行などを削除して整形しやすくする
+    
+    //運行情報
     let unko = html.match(/id="mdServiceStatus">(.*?)<\/div>/)[1];
     unko = unko.replace(/<("[^"]*"|'[^']*'|[^'">])*>/g,''); //整形2: タグを削除
     console.log(unko);
 
-    await updateData({msg: unko}); //データ更新関数を実行
+    //時間
+    let jikan = html.match(/class="subText">(.*?)<\/span>/)[1];
+    jikan = jikan.replace(/<("[^"]*"|'[^']*'|[^'">])*>/g,''); //整形2: タグを削除
+    console.log(jikan);
+
+    const saveData = {
+        date: jikan,
+        msg: unko
+    }
+
+    await updateData(saveData); //データ更新関数を実行
   } catch (error) {
     console.error(error);
   }
